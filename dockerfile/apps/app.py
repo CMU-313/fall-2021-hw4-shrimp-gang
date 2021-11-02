1bf9e5b29355eb6b6ecdacf52c6bfa38cf302133
Fernanda <fmolinag@andrew.cmu.edu>
2021-11-02 18:03:37 -0400

    Adding app.py file that checks for missing parameters and wrong parameter types

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -11,21 +11,30 @@ def hello():
 
 @app.route('/predict')
 def predict():
-	 #use entries from the query string here but could also use json
-     studytime = request.args.get('studytime')
-     absences = request.args.get('absences')
-     goout = request.args.get('goout')
-     dalc = request.args.get('Dalc')
-     G1 = request.args.get('G1')
-     G2 = request.args.get('G2')
+    required_params = ['studytime', 'absences', 'goout', 'Dalc', 'G1', 'G2']
+    for param in required_params:
+        #checks for missing parameter
+        if not param in request.args:
+            return "Missing parameter: " + param, 400
+
+        #checks for wrong parameter type
+        if request.args.get(param, type=float) is None:
+            return f"Invalid type for {param}. Expected int/float.", 400
+
+    studytime = request.args.get('studytime', type = int)
+    absences = request.args.get('absences', type = int)
+    goout = request.args.get('goout', type = int)
+    dalc = request.args.get('Dalc', type = int)
+    G1 = request.args.get('G1', type = float)
+    G2 = request.args.get('G2', type = float)
 
 
-     data = [[studytime],[absences],[dalc],[goout],[G1],[G2]]
-     query_df = pd.DataFrame({ 'studytime' : pd.Series(studytime) ,'goout' : pd.Series(goout),'Dalc' : pd.Series(dalc), 
+    data = [[studytime],[absences],[dalc],[goout],[G1],[G2]]
+    query_df = pd.DataFrame({ 'studytime' : pd.Series(studytime) ,'goout' : pd.Series(goout),'Dalc' : pd.Series(dalc), 
      'absences' : pd.Series(absences) ,'G1' : pd.Series(G1), 'G2' : pd.Series(G2)
     })
-     prediction = clf.predict(query_df)
-     return jsonify(np.asscalar(prediction))     
+    prediction = clf.predict(query_df)
+    return jsonify(np.asscalar(prediction))  
 
 if __name__ == '__main__':
     clf = joblib.load('/apps/model1.pkl')
