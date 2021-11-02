d24979eb0b1e511f9ce20d6bcd91d666098e5d70
Michael Aspinwall <maspinwall@MacBook-Pro.local>
2021-11-02 18:22:18 -0400

    Added test script with extra outputs

--- test_script.sh
+++ test_script.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 output=$(curl http://localhost:5000/predict\?studytime\=2\&absences\=4\&goout\=4\&Dalc\=1\&G1\=20\&G2\=20)
-if (($output == 1)) 
+output2=$(curl http://localhost:5000/predict\?studytime\=2\&absences\=4\&goout\=4\&Dalc\=1\&G1\=20\&G2\=0)
+output3=$(curl http://localhost:5000/predict\?studytime\=2\&absences\=0\&goout\=4\&Dalc\=1\&G1\=0\&G2\=0)
+output4=$(curl http://localhost:5000/predict\?studytime\=0\&absences\=0\&goout\=2\&Dalc\=2\&G1\=0\&G2\=3)
+output5=$(curl http://localhost:5000/predict\?studytime\=3\&absences\=0\&goout\=2\&Dalc\=2\&G1\=20\&G2\=15)
+output6=$(curl http://localhost:5000/predict\?studytime\=2\&absences\=0\&goout\=2\&Dalc\=2\&G1\=17\&G2\=20)
+if (($output != 1)) 
 then
-  exit 0
-else
   exit 1
+fi
+
+if (($output2 != 0)) 
+then
+  exit 2
+fi
+
+if (($output3 != 0)) 
+then
+  exit 3
+fi
+
+
+if (($output4 != 0)) 
+then
+  exit 4
+fi
+
+if (($output5 != 1)) 
+then
+  exit 5
+fi
+
+if (($output6 != 1)) 
+then
+  exit 6
 fi
 
